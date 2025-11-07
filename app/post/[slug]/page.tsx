2d47a85c603ad45607c1c12aaf28200acaaabf5b
Maha-Sidia <sidiamaha43@gmail.com>
2025-11-07 11:11:31 +0100

    config

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -2,13 +2,62 @@ import { notFound } from "next/navigation";
 import { fetchPostBySlug } from "@/lib/fetchPostBySlug";
 import Image from "next/image";
 import { Post } from "@/types/post";
+import { Metadata } from 'next';
+
+
 
 interface PostPageProps {
   params: { slug: string } | Promise<{ slug: string }>;
 }
 
+export async function generateMetadata({ params }: PostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const post: Post | null = await fetchPostBySlug(slug);
+
+  if (!post) {
+    return {
+      title: "post non trouvé",
+      description: "Cet article n'existe pas ou a été supprimé.",
+    };
+  }
+
+  const imageUrl = post.cover
+    ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${post.cover.url}`
+    : `${process.env.NEXT_PUBLIC_SITE_URL}/bg-blog-img.png`; 
+
+  return {
+    title: post.title,
+    description: post.content?.slice(0, 150) + "...",
+    openGraph: {
+      title: post.title,
+      description: post.content?.slice(0, 150) + "...",
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/posts/${slug}`,
+      type: "article",
+      images: [
+        {
+          url: imageUrl,
+          width: 800,
+          height: 600,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post.title,
+      description: post.content?.slice(0, 150) + "...",
+      images: [imageUrl],
+    },
+  };
+}
+
+
+
+
+
 export default async function PostPage({ params }: PostPageProps) {
-  // ⚡ Si params est une Promise, il faut l'awaiter
+  
   const { slug } = await params;
 
   const post: Post | null = await fetchPostBySlug(slug);
